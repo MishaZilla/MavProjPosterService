e1bb06a8fcefff2de017568cba74cbc386e46e64
Mihail <misha.lucenko17@gmail.com>
2024-07-25 18:13:59 +0300

    Ispravlen commit

--- src/main/java/org/example/services/PosterService.java
+++ src/main/java/org/example/services/PosterService.java
@@ -21,22 +21,23 @@ public class PosterService {
         tmp[tmp.length - 1] = movie;
         movies = tmp;
     }
-        public String[] findAll() {
-            return movies;
-        }
 
-        public String[] findLast() {
-            int resultLength;
-            if (movies.length < limit) {
-                resultLength = movies.length;
-            } else {
-                resultLength = limit;
-            }
-            String[] answer = new String[resultLength];
-            for (int i = 0; i < answer.length; i++) {
-                answer[i] = movies[movies.length - 1 - i];
-
-            }
-            return answer;
-        }
+    public String[] findAll() {
+        return movies;
     }
+
+    public String[] findLast() {
+        int resultLength;
+        if (movies.length < limit) {
+            resultLength = movies.length;
+        } else {
+            resultLength = limit;
+        }
+        String[] answer = new String[resultLength];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = movies[movies.length - 1 - i];
+
+        }
+        return answer;
+    }
+}
